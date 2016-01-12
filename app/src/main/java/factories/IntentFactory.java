91f0a5fc81d9ba8861a4aa5c595a051e8cc59fab
Pacoke <jrdlt19991@gmail.com>
2016-01-12 18:35:17 +0100

    update IntentFactory

--- app/src/main/java/factories/IntentFactory.java
+++ app/src/main/java/factories/IntentFactory.java
@@ -1,10 +1,9 @@
-package Factories;
+package factories;
 
 import android.content.Context;
 import android.content.Intent;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Javi on 12/12/2015.
@@ -14,9 +13,13 @@ public class IntentFactory {
     private static HashMap<String,Intent> intents = new HashMap<String,Intent>();
 
     public static Intent createIntent(Context contexto,Class clase){
-        Intent intent = new Intent(contexto,clase);
-        intents.put(clase.getName(),intent);
-        return intent;
+        if(intents.containsKey(clase.getName())){
+            return intents.get(clase.getName());
+        }else{
+            Intent intent = new Intent(contexto,clase);
+            intents.put(clase.getName(),intent);
+            return intent;
+        }
     }
 
     public static Intent getFactory(String nameclass){
